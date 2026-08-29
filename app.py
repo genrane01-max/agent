d37ea99d0f93e50799b4e78ef579883fce61166c
genrane01-max <genrane01@gmail.com>
2026-08-29 16:41:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def dashboard():
     return "<h1>Bot Expert
-            Dashboard</h1><p>Status: Online</p>"
+              Dashboard</h1><p>Status: Online</p>"
 
 if __name__=="__main__":
     app.run(debug=True)
